88e744a3f7ef3909b27aa137d63b4a0c09e17b67
I516430 <shivesh.navin@sap.com>
2020-02-22 20:08:41 +0530

    Initial Commit

---
+++ webapp/controller/Home.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("localui5run.localui5run.controller.Home", {
+		onInit: function () {
+
+		}
+	});
+});
